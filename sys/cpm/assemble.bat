1ef810af065fcb8e05bfc90d6f6caf3b0411762a
dj <djkunkel@gmail.com>
2026-07-26 23:07:10 -0500

    Update cpm build to unix

--- sys/cpm/assemble.bat
+++
@@ -1,8 +0,0 @@
-zmac -c --oo cim,lst cbios.asm
-zmac --oo cim,lst cpmloader.asm
-zmac --oo cim,lst format.asm
-zmac -8 --oo cim,lst pcget.asm
-zmac -8 --oo cim,lst pcput.asm
-zmac -c --oo cim,lst putsys.asm
-zmac -c --oo cim,lst cpm22.asm
-zmac --oo cim,lst savestub.asm
